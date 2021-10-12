681eeee83e5a5bd169ec7548cce3645fb90de00e
mohammad <mohammadmajidi27@gmail.com>
2021-10-12 23:22:11 +0330

    second commit

--- hardNumber.js
+++ hardNumber.js
@@ -1,29 +1,25 @@
-function isPrime(n) {
-    for (let i = 2; i < n; i++) {
-        if (n % i === 0) {
-            return false
-        }
-    }
-    if (n === 1) return false
-    else return true
-}
-
-function hardNum(n){
-    let strnumber=n.toString();
-    let m =strnumber.length;
-    for (let i=0 ; i<m;i++){
-        if (!isPrime(Number(strnumber.substring(0,m-1)))){
-        return false;
-        }
+function isPrime(a){
+    if (a===2)  return true
+    if (a===1) return false
+    for(let i=2;i<=((a/2)+1);i++){
+        if (a%i===0) return false
     }
     return true
-
 }
-function hardPass(n){
-    for (let i=10**(n-1);i<10**n;i++){
-        if(hardNum(i)){
-            console.log(i)
+function isHard(i) {
+    let iCopy = i;
+    while (isPrime(iCopy)){
+        iCopy=(iCopy-iCopy%10)/10
+        if (iCopy<=10){
+            if (isPrime(iCopy)) return true;
         }
     }
+    return false
+
 }
-hardPass(52)
+let n=3
+let num=10**(n-1)
+let maxLim=(10**n)
+for (num;num<maxLim;num++){
+   if(isHard(num)) console.log(num)
+}
